7f5cc7f2eac446329c21c6ac575012c5e6f27701
James Sullivan <james.sullivan@live.co.uk>
2021-03-18 07:27:09 +0000

    updated gitignore

---
+++ gatsby/.cache/async-requires.js
@@ -0,0 +1,15 @@
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+exports.components = {
+  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
+  "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
+  "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
+  "component---src-pages-approach-js": () => import("./../../../src/pages/approach.js" /* webpackChunkName: "component---src-pages-approach-js" */),
+  "component---src-pages-clients-js": () => import("./../../../src/pages/clients.js" /* webpackChunkName: "component---src-pages-clients-js" */),
+  "component---src-pages-contact-js": () => import("./../../../src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
+  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
+  "component---src-pages-news-js": () => import("./../../../src/pages/news.js" /* webpackChunkName: "component---src-pages-news-js" */),
+  "component---src-pages-projects-js": () => import("./../../../src/pages/projects.js" /* webpackChunkName: "component---src-pages-projects-js" */)
+}
+
---
+++ gatsby/.cache/sync-requires.js
@@ -0,0 +1,18 @@
+const { hot } = require("react-hot-loader/root")
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/jamessullivan/Dropbox/Code/private work/Gatsby-tpr/tpr-gatsby/gatsby/.cache/dev-404-page.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("/Users/jamessullivan/Dropbox/Code/private work/Gatsby-tpr/tpr-gatsby/gatsby/src/pages/404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/jamessullivan/Dropbox/Code/private work/Gatsby-tpr/tpr-gatsby/gatsby/src/pages/about.js"))),
+  "component---src-pages-approach-js": hot(preferDefault(require("/Users/jamessullivan/Dropbox/Code/private work/Gatsby-tpr/tpr-gatsby/gatsby/src/pages/approach.js"))),
+  "component---src-pages-clients-js": hot(preferDefault(require("/Users/jamessullivan/Dropbox/Code/private work/Gatsby-tpr/tpr-gatsby/gatsby/src/pages/clients.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("/Users/jamessullivan/Dropbox/Code/private work/Gatsby-tpr/tpr-gatsby/gatsby/src/pages/contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/jamessullivan/Dropbox/Code/private work/Gatsby-tpr/tpr-gatsby/gatsby/src/pages/index.js"))),
+  "component---src-pages-news-js": hot(preferDefault(require("/Users/jamessullivan/Dropbox/Code/private work/Gatsby-tpr/tpr-gatsby/gatsby/src/pages/news.js"))),
+  "component---src-pages-projects-js": hot(preferDefault(require("/Users/jamessullivan/Dropbox/Code/private work/Gatsby-tpr/tpr-gatsby/gatsby/src/pages/projects.js")))
+}
+
